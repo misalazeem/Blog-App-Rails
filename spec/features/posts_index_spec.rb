26009d020fca14e7bf8ca28ef1b10ad99b3a31cf
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-08-29 18:31:10 +0500

    fixing n+1 problems and adding specs for posts index and posts show

---
+++ spec/features/posts_index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Post Index', type: :feature do
+  let(:user) { User.create(name: 'Tom', photo: 'https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg', bio: 'Tom is from Spain') }
+  let!(:post) { Post.create(author: user, title: "first post", text: 'Hello World') }
+  let!(:comment1) { Comment.create(author: user, post:, text: 'Hello') }
+  let!(:comment2) { Comment.create(author: user, post:, text: 'Hi') }
+  let!(:comment3) { Comment.create(author: user, post:, text: 'Hey') }
+  let!(:like1) { Like.create(author: user, post:) }
+
+  scenario "See users profile and post interactions" do
+    visit user_posts_path(user)
+    expect(page).to have_content('Tom')
+    expect(page).to have_css("img[alt='Tom']", count: 1)
+    expect(page).to have_content('Number of posts: 1')
+    expect(page).to have_content('Comments: 3')
+    expect(page).to have_content('Likes: 1')
+  end
+
+  scenario "See post title/body/comments" do
+    visit user_posts_path(user)
+    expect(page).to have_content('Hello World')
+    expect(page).to have_content('Hello')
+    expect(page).to have_content("first post")
+  end
+
+  scenario "Redirecting to post page when click on the post" do
+    visit user_posts_path(user)
+    click_link "first post"
+    expect(page).to have_current_path(user_post_path(user, post))
+  end
+end
---
+++ spec/features/posts_show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature 'Post Show', type: :feature do
+  let(:user) { User.create(name: 'Tom', photo: 'https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg', bio: 'Tom is from Spain') }
+  let!(:post) { Post.create(author: user, title: "first post", text: 'This is the first post') }
+  let!(:post2) { Post.create(author: user, title: 'second post', text: 'This is the second post') }
+  let!(:post3) { Post.create(author: user, title: 'third post', text: 'This is the third post') }
+  let!(:post4) { Post.create(author: user, title: 'forth post', text: 'This is the forth post') }
+  let!(:comment1) { Comment.create(author: user, post:, text: 'Hello') }
+  let!(:comment2) { Comment.create(author: user, post:, text: 'Hi') }
+  let!(:comment3) { Comment.create(author: user, post:, text: 'Hey') }
+  let!(:like1) { Like.create(author: user, post:) }
+
+  scenario 'View the post and its interactions' do
+    visit user_post_path(user, post)
+    expect(page).to have_content("first post")
+    expect(page).to have_content('by Tom')
+    expect(page).to have_content('Comments: 3')
+    expect(page).to have_content('Likes: 1')
+  end
+
+  scenario "Post body view" do
+    visit user_post_path(user, post)
+    expect(page).to have_content('This is the first post')
+  end
+
+  scenario 'Checking comment views' do
+    user2 = User.create(name: 'Lily')
+    user3 = User.create(name: 'Misal')
+    Comment.create(author: user2, post:, text: 'fifth comment')
+    Comment.create(author: user3, post:, text: 'sixth comment')
+
+    visit user_post_path(user, post)
+
+    expect(page).to have_content('Lily')
+    expect(page).to have_content('Misal')
+    expect(page).to have_content('sixth comment')
+  end
+end
